7c766fe5b8754036a6e5d5a8314fd8e47ae58e24
CtrlVaultDel <ctrlvaultdel51@gmail.com>
2023-03-01 13:06:29 -0600

    Initial Commit

---
+++ TempConverter/TempConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TempConverterApp.swift
+//  TempConverter
+//
+//  Created by Ryan DeVault on 3/1/23.
+//
+
+import SwiftUI
+
+@main
+struct TempConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
